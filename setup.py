db1350753778665dea51d8ccc425e31571e23e5c
Guyzmo <guyzmo+github@m0g.net>
2016-03-21 21:17:49 +0100

    Added build files
    Signed-off-by: Guyzmo <guyzmo+github@m0g.net>

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+setup(name='git-repo',
+      version='1.0',
+      description='tool for managing remote repositories from your git CLI!',
+      classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+      ],
+      keywords='git',
+      url='https://github.com/guyzmo/git-repo',
+      author='Bernard `Guyzmo` Pratz',
+      author_email='guyzmo+git-repo@m0g.net',
+      setup_requires=['setuptools-markdown'],
+      long_description_mackdown_filename='README.md',
+      install_requires=[
+            'docopt',
+            'GitPython',
+            'python-gitlab',
+            'github3.py',
+            'bitbucket-api',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [console_scripts]
+      git-repo = git_repo.repo:cli
+      """,
+      license='GPLv2',
+      packages=find_packages(),
+      # test_suite='nose.collector',
+      # tests_require=['nose'],
+      zip_safe=False
+      )
