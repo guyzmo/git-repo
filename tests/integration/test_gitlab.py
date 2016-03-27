79908d3e5a8f998bd7c6468f888056daf77a066d
Guyzmo <guyzmo+github@m0g.net>
2016-03-27 20:48:33 +0200

    Added gitlab tests (WIP)

--- tests/integration/test_gitlab.py
+++ tests/integration/test_gitlab.py
@@ -19,6 +19,8 @@ log = logging.getLogger('{red}test.gitlab{reset}'.format(**fmt))
 
 #################################################################################
 
+import sys
+
 from tests.helpers import GitRepoTestCase
 
 from git_repo.services.service import gitlab
@@ -37,85 +39,85 @@ class Test_Gitlab(GitRepoTestCase):
         return gitlab.gitlab.requests
 
     def test_00_fork(self):
-        self.action_fork(cassette_name=__name__,
+        self.action_fork(cassette_name=sys._getframe().f_code.co_name,
                          local_namespace='guyzmo',
                          remote_namespace='sigmavirus24',
                          repository='github3-py')
 
     def test_01_create(self):
-        self.action_create(cassette_name=__name__,
+        self.action_create(cassette_name=sys._getframe().f_code.co_name,
                            namespace='guyzmo',
                            repository='foobar')
 
 
     def test_02_delete(self):
-        self.action_delete(cassette_name=__name__,
+        self.action_delete(cassette_name=sys._getframe().f_code.co_name,
                            namespace='guyzmo',
                            repository='foobar')
 
     def test_03_delete_nouser(self):
-        self.action_delete(cassette_name=__name__,
+        self.action_delete(cassette_name=sys._getframe().f_code.co_name,
                            repository='github3-py')
 
     def test_04_clone(self):
-        self.action_clone(cassette_name=__name__,
+        self.action_clone(cassette_name=sys._getframe().f_code.co_name,
                           namespace='guyzmo',
                           repository='git-repo')
 
     def test_05_add(self):
-        self.action_add(cassette_name=__name__,
+        self.action_add(cassette_name=sys._getframe().f_code.co_name,
                         namespace='guyzmo',
                         repository='git-repo')
 
     def test_06_add__name(self):
-        self.action_add(cassette_name=__name__,
+        self.action_add(cassette_name=sys._getframe().f_code.co_name,
                         namespace='guyzmo',
                         repository='git-repo',
                         name='test0r')
 
     def test_07_add__alone(self):
-        self.action_add(cassette_name=__name__,
+        self.action_add(cassette_name=sys._getframe().f_code.co_name,
                         namespace='guyzmo',
                         repository='git-repo',
                         alone=True)
 
     def test_08_add__alone_name(self):
-        self.action_add(cassette_name=__name__,
+        self.action_add(cassette_name=sys._getframe().f_code.co_name,
                         namespace='guyzmo',
                         repository='git-repo',
                         name='test0r',
                         alone=True)
 
     def test_09_add__default(self):
-        self.action_add(cassette_name=__name__,
+        self.action_add(cassette_name=sys._getframe().f_code.co_name,
                         namespace='guyzmo',
                         repository='git-repo',
-                        default=True)
+                        tracking='gitlab')
 
     def test_10_add__default_name(self):
-        self.action_add(cassette_name=__name__,
+        self.action_add(cassette_name=sys._getframe().f_code.co_name,
                         namespace='guyzmo',
                         repository='git-repo',
                         name='test0r',
-                        default=True)
+                        tracking='gitlab')
 
     def test_11_add__alone_default(self):
-        self.action_add(cassette_name=__name__,
+        self.action_add(cassette_name=sys._getframe().f_code.co_name,
                         namespace='guyzmo',
                         repository='git-repo',
                         alone=True,
-                        default=True)
+                        tracking='gitlab')
 
     def test_12_add__alone_default_name(self):
-        self.action_add(cassette_name=__name__,
+        self.action_add(cassette_name=sys._getframe().f_code.co_name,
                         namespace='guyzmo',
                         repository='git-repo',
                         alone=True,
                         name='test0r',
-                        default=True)
+                        tracking='gitlab')
 
     def test_13_open(self):
-        self.action_open(cassette_name=__name__,
+        self.action_open(cassette_name=sys._getframe().f_code.co_name,
                          namespace='guyzmo',
                          repository='git-repo')
 
