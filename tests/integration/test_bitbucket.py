5421a55f5ab751fdaab66154ac41988da54b4aa0
Guyzmo <guyzmo+github@m0g.net>
2016-03-29 20:02:37 +0200

    Updated bitbucket tests suite

--- tests/integration/test_bitbucket.py
+++ tests/integration/test_bitbucket.py
@@ -9,13 +9,11 @@ log = logging.getLogger('test.bitbucket')
 
 #################################################################################
 
+import sys
+
 from tests.helpers import GitRepoTestCase
 
 from git_repo.services.service import bitbucket
-
-# monkey patch requests with session
-from requests import Session
-bitbucket.bitbucket.requests = Session()
 
 class Test_BitBucket(GitRepoTestCase):
     log = log
@@ -24,88 +22,88 @@ class Test_BitBucket(GitRepoTestCase):
         return bitbucket.BitbucketService(c=dict())
 
     def get_requests_session(self):
-        return bitbucket.bitbucket.requests
+        return self.service.bb.session
 
     def test_00_fork(self):
-        self.action_fork(cassette_name=__name__,
+        self.action_fork(cassette_name=sys._getframe().f_code.co_name,
                          local_namespace='guyzmo',
                          remote_namespace='abdo2015',
                          repository='git_tutorial')
 
     def test_01_create(self):
-        self.action_create(cassette_name=__name__,
+        self.action_create(cassette_name=sys._getframe().f_code.co_name,
                            namespace='guyzmo',
                            repository='foobar')
 
 
     def test_02_delete(self):
-        self.action_delete(cassette_name=__name__,
+        self.action_delete(cassette_name=sys._getframe().f_code.co_name,
                            namespace='guyzmo',
                            repository='foobar')
 
     def test_03_delete_nouser(self):
-        self.action_delete(cassette_name=__name__,
-                           repository='github3.py')
+        self.action_delete(cassette_name=sys._getframe().f_code.co_name,
+                           repository='git_tutorial')
 
     def test_04_clone(self):
-        self.action_clone(cassette_name=__name__,
+        self.action_clone(cassette_name=sys._getframe().f_code.co_name,
                           namespace='guyzmo',
                           repository='git-repo')
 
     def test_05_add(self):
-        self.action_add(cassette_name=__name__,
+        self.action_add(cassette_name=sys._getframe().f_code.co_name,
                         namespace='guyzmo',
                         repository='git-repo')
 
     def test_06_add__name(self):
-        self.action_add(cassette_name=__name__,
+        self.action_add(cassette_name=sys._getframe().f_code.co_name,
                         namespace='guyzmo',
                         repository='git-repo',
                         name='test0r')
 
     def test_07_add__alone(self):
-        self.action_add(cassette_name=__name__,
+        self.action_add(cassette_name=sys._getframe().f_code.co_name,
                         namespace='guyzmo',
                         repository='git-repo',
                         alone=True)
 
     def test_08_add__alone_name(self):
-        self.action_add(cassette_name=__name__,
+        self.action_add(cassette_name=sys._getframe().f_code.co_name,
                         namespace='guyzmo',
                         repository='git-repo',
                         name='test0r',
                         alone=True)
 
     def test_09_add__default(self):
-        self.action_add(cassette_name=__name__,
+        self.action_add(cassette_name=sys._getframe().f_code.co_name,
                         namespace='guyzmo',
                         repository='git-repo',
-                        default=True)
+                        tracking='bitbucket')
 
     def test_10_add__default_name(self):
-        self.action_add(cassette_name=__name__,
+        self.action_add(cassette_name=sys._getframe().f_code.co_name,
                         namespace='guyzmo',
                         repository='git-repo',
                         name='test0r',
-                        default=True)
+                        tracking='bitbucket')
 
     def test_11_add__alone_default(self):
-        self.action_add(cassette_name=__name__,
+        self.action_add(cassette_name=sys._getframe().f_code.co_name,
                         namespace='guyzmo',
                         repository='git-repo',
                         alone=True,
-                        default=True)
+                        tracking='bitbucket')
 
     def test_12_add__alone_default_name(self):
-        self.action_add(cassette_name=__name__,
+        self.action_add(cassette_name=sys._getframe().f_code.co_name,
                         namespace='guyzmo',
                         repository='git-repo',
                         alone=True,
                         name='test0r',
-                        default=True)
+                        tracking='bitbucket')
 
     def test_13_open(self):
-        self.action_open(cassette_name=__name__,
+        self.action_open(cassette_name=sys._getframe().f_code.co_name,
                          namespace='guyzmo',
                          repository='git-repo')
 
