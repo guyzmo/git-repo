3ed1a092bd981b0f503d1fd90cea93e4b412d6e0
Guyzmo <guyzmo+github@m0g.net>
2016-10-15 16:54:38 +0200

    🚒 Fixed issues with conftest
    * Added support for token/private_key/privatekey config setting ;
    * Added support for non static services list (will help when
      we'll support more services) ;
    * made betamax configuration dynamic

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,36 +7,43 @@ from betamax_serializers import pretty_json
 
 record_mode = 'once'
 
+from git_repo.services.service import RepositoryService
+services = list(RepositoryService.service_map.keys())
+
 if os.environ.get('TRAVIS_GH3'):
     # create default bogus values for tokens and namespaces if missing for pytest
     # to run without environment values
     # also if an environment variable is not set, then we don't want to record cassettes
     record_mode = 'never'
-    for service_name in ('github', 'gitlab', 'bitbucket'):
-        token_name = 'PRIVATE_KEY_{}'.format(service_name.upper())
-        namespace_name = '{}_NAMESPACE'.format(service_name.upper())
-        if token_name not in os.environ:
-            os.environ[token_name] = 'not_configured:test' # using a : for bitbucket's case
-        if namespace_name not in os.environ:
-            os.environ[namespace_name] = 'not_configured'
-else:
-    import git, getpass
-    config = git.config.GitConfigParser(os.path.join(os.environ['HOME'], '.gitconfig'))
-    conf_section = list(filter(lambda n: 'gitrepo' in n, config.sections()))
-    key_dict = {section.split('"')[1] :config.get_value(section, 'privatekey') for section in conf_section}
-    for service, key in key_dict.items():
+    for service in services:
         token_name = 'PRIVATE_KEY_{}'.format(service.upper())
         namespace_name = '{}_NAMESPACE'.format(service.upper())
-        os.environ[token_name] = key
-        os.environ[namespace_name] = os.environ.get('GITREPO_NAMESPACE', getpass.getuser())
+        if token_name not in os.environ:
+            os.environ[token_name] = '_token_{}_:_private_'.format(s) # using a : for bitbucket's case
+        if namespace_name not in os.environ:
+            os.environ[namespace_name] = '_not_a_namespace_'
+else:
+    # if running tests "locally" and not in travis, let's try to extract the keys from
+    # the local configuration if there is some local configuration. And exposes them as
+    # environment variables.
+    import git, getpass
+    config = git.config.GitConfigParser(os.path.join(os.environ['HOME'], '.gitconfig'))
 
-api_token_github = os.environ['PRIVATE_KEY_GITHUB']
-api_token_gitlab = os.environ['PRIVATE_KEY_GITLAB']
-api_token_bitbucket = os.environ['PRIVATE_KEY_BITBUCKET']
+    # handle the different forms of token configuration item (yup, technical debt bites here)
+    get_section = lambda s: 'gitrepo "{}"'.format(s)
+    get_token = lambda s: config.get_value(get_section(s), 'token',
+                            config.get_value(get_section(s), 'private_token',
+                                config.get_value(get_section(s), 'privatekey',
+                                        '_token_{}_:_private_'.format(s) # using a : for bitbucket's case
+                                    )))
 
-github_namespace = os.environ['GITHUB_NAMESPACE']
-gitlab_namespace = os.environ['GITLAB_NAMESPACE']
-bitbucket_namespace = os.environ['BITBUCKET_NAMESPACE']
+    for service in services:
+        token_name = 'PRIVATE_KEY_{}'.format(service.upper())
+        namespace_name = '{}_NAMESPACE'.format(service.upper())
+        if token_name not in os.environ:
+            os.environ[token_name] = get_token(service)
+        if namespace_name not in os.environ:
+            os.environ[namespace_name] = os.environ.get('GITREPO_NAMESPACE', getpass.getuser())
 
 betamax.Betamax.register_serializer(pretty_json.PrettyJSONSerializer)
 
@@ -44,12 +51,8 @@ with betamax.Betamax.configure() as config:
     config.default_cassette_options['record_mode'] = record_mode
     config.cassette_library_dir = 'tests/integration/cassettes'
     config.default_cassette_options['serialize_with'] = 'prettyjson'
-    config.define_cassette_placeholder('<PRIVATE_KEY_GITHUB>', api_token_github)
-    config.define_cassette_placeholder('<PRIVATE_KEY_GITLAB>', api_token_gitlab)
-    config.define_cassette_placeholder('<PRIVATE_KEY_BITBUCKET>', api_token_bitbucket)
-    config.define_cassette_placeholder('<GITHUB_NAMESPACE>', github_namespace)
-    config.define_cassette_placeholder('<GITLAB_NAMESPACE>', gitlab_namespace)
-    config.define_cassette_placeholder('<BITBUCKET_NAMESPACE>', bitbucket_namespace)
-
-
+    # generating placeholders in betamax configuration for each service's key and default namespace
+    for service in services:
+        config.define_cassette_placeholder('<PRIVATE_KEY_{}>'.format(service.upper()), os.environ.get('PRIVATE_KEY_{}'.format(service.upper())))
+        config.define_cassette_placeholder('<{}_NAMESPACE>'.format(service.upper()), os.environ.get('{}_NAMESPACE'.format(service.upper())))
 
