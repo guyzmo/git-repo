5f5ac01d5e12128c977528218252bfb7c01c6277
Guyzmo <guyzmo+github@m0g.net>
2016-12-02 17:11:08 +0100

    🚧 Updated

--- git_repo/services/ext/bitbucket.py
+++ git_repo/services/ext/bitbucket.py
@@ -6,20 +6,13 @@ log = logging.getLogger('git_repo.bitbucket')
 from ..service import register_target, RepositoryService
 from ...exceptions import ResourceError, ResourceExistsError, ResourceNotFoundError
 
-from pybitbucket.bitbucket import Client
+from pybitbucket.bitbucket import Client, Bitbucket
 from pybitbucket.auth import BasicAuthenticator
-from pybitbucket.user import User
 from pybitbucket.repository import Repository, RepositoryForkPolicy
 
 from requests import Request, Session
 from requests.exceptions import HTTPError
 import json
-
-# bitbucket.URLS.update({
-#     'GET_REPO' : 'repositories/%(username)s/%(repo_slug)s/',
-#     'DELETE_REPO' : 'repositories/%(accountname)s/%(repo_slug)s',
-#     'FORK_REPO' : 'repositories/%(username)s/%(repo_slug)s/fork',
-# })
 
 
 @register_target('bb', 'bitbucket')
@@ -27,7 +20,8 @@ class BitbucketService(RepositoryService):
     fqdn = 'bitbucket.org'
 
     def __init__(self, *args, **kwarg):
-        self.bb = Client()
+        self.bb_client = Client()
+        self.bb = Bitbucket(self.bb_client)
         super(BitbucketService, self).__init__(*args, **kwarg)
 
     def connect(self):
@@ -35,11 +29,11 @@ class BitbucketService(RepositoryService):
             raise ConnectionError('Could not connect to BitBucket. Please configure .gitconfig with your bitbucket credentials.')
         if not ':' in self._privatekey:
             raise ConnectionError('Could not connect to BitBucket. Please setup your private key with login:password')
-        self.bb.config = BasicAuthenticator(*self._privatekey.split(':')+['z+git-repo+pub@m0g.net'])
-        self.bb.session = self.bb.config.session
+        self.bb_client.config = BasicAuthenticator(*self._privatekey.split(':')+['z+git-repo+pub@m0g.net'])
+        self.bb_client.session = self.bb_client.config.session
         try:
-            User.find_current_user(client=self.bb)
-        except HTTPError as err:
+            self.user
+        except ResourceError as err:
             raise ConnectionError('Could not connect to BitBucket. Not authorized, wrong credentials.') from err
 
     def create(self, user, repo, add=False):
@@ -48,36 +42,36 @@ class BitbucketService(RepositoryService):
                     repo,
                     fork_policy=RepositoryForkPolicy.ALLOW_FORKS,
                     is_private=False,
-                    client=self.bb
+                    client=self.bb_client
             )
             if add:
                 self.add(user=user, repo=repo, tracking=self.name)
         except HTTPError as err:
-            if err.status_code == 400:
+            if '400' in err.args[0].split(' '):
                 raise ResourceExistsError('Project {} already exists on this account.'.format(repo)) from err
             raise ResourceError("Couldn't complete creation: {}".format(err)) from err
 
     def fork(self, user, repo):
         raise NotImplementedError('No support yet by the underlying library.')
         try:
-            Repository.find_repository_by_name_and_owner(repo, owner=user, client=self.bb).fork()
+            self.get_repository(user, repo).fork()
         except HTTPError as err:
-            raise ResourceError("Couldn't complete creation: {}".format(err)) from err
+            raise ResourceError("Couldn't complete fork: {}".format(err)) from err
         return '/'.join([result['owner'], result['slug']])
 
     def delete(self, repo, user=None):
         if not user:
             user = self.user
         try:
-            Repository.find_repository_by_name_and_owner(repo, owner=user, client=self.bb).delete()
+            self.get_repository(user, repo).delete()
         except HTTPError as err:
-            if err.status_code == 404:
+            if '404' in err.args[0].split(' '):
                 raise ResourceNotFoundError("Cannot delete: repository {}/{} does not exists.".format(user, repo)) from err
-            raise ResourceError("Couldn't complete creation: {}".format(err)) from err
+            raise ResourceError("Couldn't complete deletion: {}".format(err)) from err
 
     def get_repository(self, user, repo):
         try:
-            return Repository.find_repository_by_name_and_owner(repo, owner=user, client=self.bb)
+            return next(self.bb.repositoryByOwnerAndRepositoryName(owner=user, repository_name=repo))
         except HTTPError as err:
             raise ResourceNotFoundError('Cannot retrieve repository: {}/{} does not exists.'.format(user, repo))
 
@@ -89,8 +83,9 @@ class BitbucketService(RepositoryService):
     @property
     def user(self):
         try:
-            return User.find_current_user(client=self.bb).username
-        except HTTPError as err:
-            raise ResourceError("Couldn't complete creation: {}".format(err)) from err
+            user = next(self.bb.userForMyself()).username
+            return user
+        except (HTTPError, AttributeError) as err:
+            raise ResourceError("Couldn't find the current user: {}".format(err)) from err
 
 
