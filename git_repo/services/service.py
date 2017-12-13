2fb6d4f344386c8bd1d0a0738323619bc8d4e526
Phantom42 <nikitav30@gmail.com>
2017-12-13 20:35:15 +0200

    Add gerrit review support

--- git_repo/services/service.py
+++ git_repo/services/service.py
@@ -11,6 +11,7 @@ import webbrowser
 from git import RemoteProgress, config as git_config
 from progress.bar import IncrementalBar as Bar
 
+from enum import Enum
 from urllib.parse import ParseResult
 from subprocess import call
 
@@ -32,8 +33,18 @@ EXTRACT_URL_RE = re.compile('[^:]*(://|@)[^/]*/')
 
 class ProgressBar(RemoteProgress): # pragma: no cover
     '''Nice looking progress bar for long running commands'''
-    def setup(self, repo_name):
-        self.bar = Bar(message='Pulling from {}'.format(repo_name), suffix='')
+
+    class Action(Enum):
+        PULL = 1
+        PUSH = 2
+
+    def setup(self, repo_name, action=Action.PULL):
+        if action == ProgressBar.Action.PULL:
+            message = 'Pulling from {}'.format(repo_name)
+        elif action == ProgressBar.Action.PUSH:
+            message = 'Pushing to {}'.format(repo_name)
+
+        self.bar = Bar(message=message, suffix='')
 
     def update(self, op_code, cur_count, max_count=100, message=''):
         #log.info("{}, {}, {}, {}".format(op_code, cur_count, max_count, message))
@@ -310,6 +321,21 @@ class RepositoryService:
         else: # pragma: no cover
             remote.pull(progress=pb)
         print()
+
+    def push(self, remote, branch=None):
+        '''Push a repository
+        :param remote: git-remote instance
+        :param branch: name of the branch to push
+        :return: PushInfo, git push output lines
+        '''
+        pb = ProgressBar()
+        pb.setup(self.name, ProgressBar.Action.PUSH)
+        if branch:
+            result = remote.push(branch, progress=pb)
+        else: #pragma: no cover
+            result = remote.push(progress=pb)
+        print()
+        return result, pb.other_lines
 
     def fetch(self, remote, remote_branch, local_branch, force=False):
         '''Pull a repository
